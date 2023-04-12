2de342700cfe2e5a26c59157e03cd2abfb9fba60
valerythedev <valgonzr@gmail.com>
2023-04-11 23:14:05 -0400

    update

--- 8kyu/BMI/bmi.js
+++ 8kyu/BMI/bmi.js
@@ -10,13 +10,13 @@
 
 function bmi(weight, height) {
   bmi = weight / height ** 2
-  if (bmi <= 18.5) {
+  if (bmii <= 18.5) {
     return "Underweight"
-  } else if (bmi <= 25.0) {
+  } else if (bmii <= 25.0) {
     return "Normal"
-  } else if (bmi <= 30.0) {
+  } else if (bmii <= 30.0) {
     return "Overweight"
-  } else if (bmi > 30) {
+  } else if (bmii > 30) {
     return "Obese"
   }
 }
