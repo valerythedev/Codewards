7b9f68d3591829786295b16324d4c38836e68404
valerythedev <valgonzr@gmail.com>
2023-05-02 13:36:26 -0400

    update

---
+++ 5kyu/practice.js
@@ -0,0 +1,12 @@
+// create function that takes a number as an argument 
+// returns odd if odd and even if it's even
+
+function oddOrEven(n){
+  if (n % 2 === 0){
+    return "even"
+  } else{
+    return "odd"
+  }
+}
+oddOrEven(6)
+console.log(oddOrEven)
