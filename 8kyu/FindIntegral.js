ec90f6f21c611e49e0b68f2e679e1b2c6cce78b7
Valery Gonzalez <valgonzr@gmail.com>
2023-05-04 14:04:58 -0400

    update

--- 8kyu/FindIntegral.js
+++ 8kyu/FindIntegral.js
@@ -2,7 +2,7 @@
 
 // In order to find the integral all you need to do is add one to the exponent (the second argument), and divide the coefficient (the first argument) by that new number.
 
-// For example for 3x^2, the integral would be 1x^3: we added 1 to the exponent, and divided the coefficient by that new number).
+// For example for 3x^2, the integral would be 1x^3: we added 1 to the exponent, and divided the coefficient by that new number). update
 
 // Notes:
 
