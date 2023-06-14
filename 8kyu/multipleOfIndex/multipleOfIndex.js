2f015b28f9b8c1426dbfa1e99c0cb1a610ba3f6f
Valery Gonzalez <valgonzr@gmail.com>
2023-06-13 22:23:47 -0400

    pseudo code

--- 8kyu/multipleOfIndex/multipleOfIndex.js
+++ 8kyu/multipleOfIndex/multipleOfIndex.js
@@ -12,7 +12,7 @@ function multipleOfIndex(array) {
 //   return new array
 // variable that contains results
 const result = [];
-  
+  // index starts at 0 
   for (let i = 0; i < array.length; i++) {
     if (array[i] === 0 || array[i] % i === 0) {
       result.push(array[i]);
